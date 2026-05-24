98246f9544d63836c568eeff5952c0e5187c50c1
YuHsuan <c.yuhsuann@gmail.com>
2026-05-24 18:09:07 +1000

    feat: update DRF, JWT auth, CORS, and filters in settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "django_filters",
+    "corsheaders",
     "finance",
 ]
 
@@ -118,3 +121,14 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 AUTH_USER_MODEL = "finance.User"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+}
